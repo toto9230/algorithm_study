672373f012480ff13c59e7893bdfb9639679db4e
Jiyoung Kim <33561224+toto9230@users.noreply.github.com>
2020-11-24 19:15:57 +0900

    Update 11-2. 곱하기 혹은 더하기.py
    0일 때 뿐만 아니라, 1 일 때도 더해야 함.

--- 이코테/그리디/11-2. 곱하기 혹은 더하기.py
+++ 이코테/그리디/11-2. 곱하기 혹은 더하기.py
@@ -1,11 +1,12 @@
 s = input()
-list_s = list(map(int, s)) #str list를 int로 바꾸고 싶을때
+list_s = list(map(int, s))  #str list를 int로 바꾸고 싶을때 
+                            # 굳이 이렇게 안하고, int(s[0])로 했어도 됨. 
 
 result = 0
 for i in range(len(s)):
-  if(result == 0 or list_s[i]==1):
-    result += list_s[i]
-  else:
-    result *=list_s[i]
+    if (result <= 1 or list_s[i] <= 1):
+        result += list_s[i]
+    else:
+        result *= list_s[i]
 
 print(result)
