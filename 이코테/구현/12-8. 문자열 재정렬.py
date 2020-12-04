37ed52caeb83eb79f7930c6be313e08854bb38d4
Jiyoung Kim <33561224+toto9230@users.noreply.github.com>
2020-12-04 16:12:56 +0900

    Update 12-8. 문자열 재정렬.py
    숫자들의 합이 0일때는 넣지 않는 코드 수정.

--- 이코테/구현/12-8. 문자열 재정렬.py
+++ 이코테/구현/12-8. 문자열 재정렬.py
@@ -1,14 +1,15 @@
 s = input()
-numbers = ['0', '1', '2', '3', '4', '5','6', '7', '8', '9']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 alphabet = []
 number = 0
 for word in s:
-  if word in numbers:
-    number += int(word)
-  else:
-    alphabet.append(word)
+    if word in numbers: #반대로 알파벳인지 체크할 때는 그냥 isalpha()함수 쓰면 됨.
+        number += int(word)
+    else:
+        alphabet.append(word)
 
 alphabet.sort()
-alphabet.append(str(number))
-print(''.join(alphabet)) # list를 string으로 바꾸는 함수 
+if number != 0:
+  alphabet.append(str(number)) #숫자의 합이 0일때는 넣지 않음.
+print(''.join(alphabet))  # list를 string으로 바꾸는 함수
